743e1ee268040ddc0867114d14b37c95c210a882
Alexey Terentyev <shamann1101@gmail.com>
2016-12-12 09:10:42 +0000

    #20 JSON to PHP парсит нормально

--- cli.php
+++ cli.php
@@ -1,6 +1,10 @@
 <?php
 require 'bootstrap.php';
 
-$var = new Hello;
+$var = new Hello();
 
 $var->TypeHello();
+echo "\n";
+$var2 = new Json\Parser();
+$path = file_get_contents(__DIR__."/Json/source.js");
+$var2->JsonToPhpParser($path);
