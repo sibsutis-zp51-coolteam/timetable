e0c5490498e27856454b18032069c5d7665a20a8
Ilya Krylatov <ilyarkl@gmail.com>
2016-12-16 12:22:53 +0000

    code review

--- scripts/Controller/Train.php
+++ scripts/Controller/Train.php
@@ -2,38 +2,47 @@
 
 namespace Controller;
 
+
+/**
+ * K/lacc co cnucKom cTaHu,uu' c/legoBaHu9|
+ */
 class Train
 {
-        public function Listing ()
-        {
-
-    $api = new \Yandex\Api();
-
-    $params = [
-    "uid" => "6630_0_9610642_g17_4",
-    "transport_types" => "suburban",
-    "event" => "arrival",
-    "lang" => "ru",
-
-     ];
-
-    $info = $api->call('thread', $params);
-
-    $result = [];
-    foreach ($info["stops"] as $station)
+    public function Listing ()
     {
-        print_r ($station);
-        $result[] = ["arrival" => $station ['arrival'], "departure" => $station ['departure'], "code" => $station ['station']['code'], "title" => $station ['station']['title']];
 
-    }
+        $api = new \Yandex\Api();
 
-    return $result;
+        $params = [
+
+            "uid" => "6630_0_9610642_g17_4",
+            "transport_types" => "suburban",
+            "event" => "arrival",
+            "lang" => "ru",
+
+        ];
+
+        $info = $api->call('thread', $params);
+
+        $result = [];
+        foreach ($info["stops"] as $station){
+
+            print_r ($station);
+            $result[] = [
+                "arrival" => $station ['arrival'],
+                "departure" => $station ['departure'],
+                "code" => $station ['station']['code'],
+                "title" => $station ['station']['title']
+            ];
 
         }
+
+        return $result;
+
+    }
 
     public function run ()
     {
         var_export ($this -> Listing());
     }
-
 }
